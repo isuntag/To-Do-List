3ebbf6aa0a03b664ade4ff32110c33368f772ae2
Isaac <isuntag@gmail.com>
2017-10-23 15:46:11 -0400

    modal backgrounds don't stack. Each will be 50% opacity.

--- apps/tasks_app/static/tasks_app/tasks.js
+++ apps/tasks_app/static/tasks_app/tasks.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    $('.modal_toggle').click(function(e) {
-        var id = $(this).attr('task');
-        $('#modal').load("/tasks/"+id+"/task_users");
-    });
     $('.remove_user').submit(function(e) {
         e.preventDefault();
         var taskid = $(this).attr('task');
